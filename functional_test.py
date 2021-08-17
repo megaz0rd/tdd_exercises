05c6b69e3b395140620c617c7c282bfd727e8e6f
megaz0rd <akropiewnicki@gmail.com>
2021-08-17 20:07:06 +0200

    Update .gitignore

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Użytkownik dowiaduje się o aplikacji "To-do-list" i wchodzi na stronę
 browser.get('http://127.0.0.1:8000')
 
-assert "Django" in browser.title
+# Zwraca uwagę, że tytuł strony i nagłówek zawierają słowo Listy
+assert "Listy" in browser.title
+
+# Od razu zostaje zachęcony, aby wpisać rzeczy do zrobioenia
+
+# W polu tekstowym wpisał "Zrobić zakupy"
+
+# Po naciśnieęciu klawisza Enter strona została uaktualniona i wyświetla 
+# '1: Zrobić zakupy"
