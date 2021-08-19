4e0d30204a7acf740c497c023973c32ca71e2c5f
megaz0rd <akropiewnicki@gmail.com>
2021-08-19 23:43:40 +0200

    Test funkcjonalny teraz sprawdza możliwość dodania elementu listy

--- functional_test.py
+++ functional_test.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -12,21 +14,37 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         # Użytkownik dowiaduje się o aplikacji "To-do-list" i wchodzi na stronę
         self.browser.get('http://127.0.0.1:8000')
-        self.browser.implicitly_wait(3)
 
         # Zwraca uwagę, że tytuł strony i nagłówek zawierają słowo Listy
         self.assertIn("Listy", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn("Listy", header_text)
+
+
+        # Od razu zostaje zachęcony, aby wpisać rzeczy do zrobienia
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzeczy do zrobienia'
+            )
+
+        # W polu tekstowym wpisał "Zrobić zakupy na obiad"
+        inputbox.send_keys('Zrobić zakupy na obiad')
+
+        # Po naciśnieęciu klawisza Enter strona została uaktualniona i wyświetla 
+        # '1: Zrobić zakupy na obiad'
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Zrobić zakupy na obiad' for row in rows)
+        )
+
+        # Na stronie nadal znajduje się pole tekstowe zachęcające do podania 
+        # kolejnego zadania. Użytkownik wpisuje więc 'Ugotować z zakupionych
+        # produktów obiad'
         self.fail('Zakończenie testu!')
-
-# Od razu zostaje zachęcony, aby wpisać rzeczy do zrobienia
-
-# W polu tekstowym wpisał "Zrobić zakupy na obiad"
-
-# Po naciśnieęciu klawisza Enter strona została uaktualniona i wyświetla 
-# '1: Zrobić zakupy na obiad'
-
-# Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego 
-# zadania. Użytkownik wpisuje więc 'Ugotować z zakupionych produktów obiad'
 
 # Strona ponownie została zaktualizowana i wyświetla już dwa elementy z listy 
 # do zrobienia
