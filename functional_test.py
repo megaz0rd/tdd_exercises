f284a570f5fc0097ca0f0b71e62bf7b4f59908ef
megaz0rd <akropiewnicki@gmail.com>
2021-08-23 17:58:04 +0200

    Refaktoryzacja testu funkcjonalnego

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrive_it_later(self):
         # Użytkownik dowiaduje się o aplikacji "To-do-list" i wchodzi na stronę
         self.browser.get('http://127.0.0.1:8000')
@@ -21,7 +27,6 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn("Listy", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn("Listy", header_text)
-
 
         # Od razu zostaje zachęcony, aby wpisać rzeczy do zrobienia
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         # '1: Zrobić zakupy na obiad'
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Zrobić zakupy na obiad', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Zrobić zakupy na obiad')
 
         # Na stronie nadal znajduje się pole tekstowe zachęcające do podania 
         # kolejnego zadania. Użytkownik wpisuje więc 'Ugotować z zakupionych
@@ -48,14 +50,14 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Ugotować z zakupionych produktów obiad')
         inputbox.send_keys(Keys.ENTER)
-        
+        time.sleep(3)
+
         # Strona ponownie została zaktualizowana i wyświetla już dwa elementy z
         # listy do zrobienia
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Zrobić zakupy na obiad', [row.text for row in rows])
-        self.assertIn('2: Ugotować z zakupionych produktów obiad', 
-                [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Zrobić zakupy na obiad')
+        self.check_for_row_in_list_table(
+            '2: Ugotować z zakupionych produktów obiad'
+            )
         
         # Użytkownik był ciekaw czy witryna zapamięta jego listę. Zwraca uwagę 
         # na unikatowy adres URL z tekstem
