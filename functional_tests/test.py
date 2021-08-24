823239c4f51231783596e4220284ce00c5e1b973
megaz0rd <akropiewnicki@gmail.com>
2021-08-24 18:45:07 +0200

    Stworzenie functional_tests app i użycie klasy LiveServerTestCase

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,10 +1,11 @@
 import time
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -21,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Użytkownik dowiaduje się o aplikacji "To-do-list" i wchodzi na stronę
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get(self.live_server_url)
 
         # Zwraca uwagę, że tytuł strony i nagłówek zawierają słowo Listy
         self.assertIn("Listy", self.browser.title)
@@ -68,6 +69,3 @@ class NewVisitorTest(unittest.TestCase):
         # Użytkownik kończy przygodę z listą
 
         # browser.quit()
-
-if __name__ == '__main__':
-    unittest.main()
